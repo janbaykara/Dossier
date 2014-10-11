8ce76a11e48776723130316a25b39166cc8201e1
Jan Baykara <janbaykara@gmail.com>
2014-10-11 20:20:32 +0100

    Sails generate api [User/Thing/Category] models/controllers

---
+++ api/controllers/ThingController.js
@@ -0,0 +1,11 @@
+/**
+ * ThingController
+ *
+ * @description :: Server-side logic for managing things
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
