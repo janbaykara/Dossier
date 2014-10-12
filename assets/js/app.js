0a55adee15291141367bb777ed70073dc703d57a
Jan Baykara <janbaykara@gmail.com>
2014-10-12 01:13:56 +0100

    Build framework for state-sensitive AngularJS page templating

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,45 @@ var app = angular.module('Dossier', ['ui.router', 'ngSails']);
 
 app.config(function($stateProvider, $urlRouterProvider, $sailsProvider) {
 
-  // $sailsProvider.url = 'http://foo.bar';
+  $sailsProvider.url = 'http://localhost:1337';
 
   $stateProvider
+
     .state('index', {
-      url: '/',
-      templateUrl: 'partials/index.html'
+        url: '/',
+        views: {
+            '': { templateUrl: 'partials/template.content.html' },
+            '@index': { templateUrl: 'partials/view.index.html' }
+        }
+    })
+
+    .state('home', {
+        url: '/things',
+        views: {
+            '': { templateUrl: 'partials/template.content.html' },
+            '@home': { templateUrl: 'partials/view.home.html' }
+        }
+    })
+
+    .state('thing', {
+        url: '/things/:id',
+        views: {
+            '': { templateUrl: 'partials/template.content.html' },
+            '@thing': { templateUrl: 'partials/view.thing.html' }
+        }
+    })
+
+    .state('signin', {
+        url: '/signin',
+        views: {
+            '': { templateUrl: 'partials/template.single.html' },
+            '@signin': { templateUrl: 'partials/view.signin.html' }
+        }
+    })
+
+    .state('sandbox', {
+        url: '/sandbox',
+        templateUrl: 'partials/template.sandbox.html'
     })
 
     $urlRouterProvider.otherwise('/');
