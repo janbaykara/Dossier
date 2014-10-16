0dadf29139dd7ca2b3bdb9742101415fbe620aa7
Jan Baykara <janbaykara@gmail.com>
2014-10-16 20:46:20 +0100

    Create basic category view/creation UI + functionality

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -11,3 +11,29 @@ app.controller('SigninController', function($scope, SessionService) {
   // console.log("Signing in...");
   SessionService.logout();
 });
+
+app.controller('CategoryController', function($scope, Category) {
+  // $scope.categories = Category.get({query:"name=Salaslsls"}, function() {
+  //   console.log($scope.categories)
+  // });
+  $scope.categories = Category.query();
+
+  $scope.create = function() {
+    $scope.newCategory = new Category({
+      id: null,
+      name: $scope.newCategory.name,
+      color: $scope.newCategory.color
+    });
+
+    Category.save($scope.newCategory, function() {
+      $scope.categories[$scope.categories.length] = $scope.newCategory;
+    });
+  }
+
+  $scope.delete = function(category) {
+    category.$delete(function() {
+      $scope.categories.filter(function (cat) { return cat == category }) = null;
+    });
+  }
+})
+})
