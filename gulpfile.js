87fdb4e666eb49a87fb895b30edc7da483dfe4b0
Jan Baykara <janbaykara@gmail.com>
2014-10-18 11:11:56 +0100

    Tidy up Gulpfile & Package.json
    - Replace uglify with uglifyjs
    - Remove image min for it's whopping 70MB space
    - Remove unused gulp/sails packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@
 var gulp = require('gulp')
   , gulpLoadPlugins = require('gulp-load-plugins')
   , mainBowerFiles = require('main-bower-files')
-  , browserSync = require('browser-sync')
-  , browserReload = browserSync.reload
 
 // Autoload Gulp tasks from package.json
 var plugins = gulpLoadPlugins();
@@ -47,23 +45,13 @@ gulp.task('install', function () {
 
   gulp.task('css', function () {
     return gulp.src(dirs.dev.sass)
-    .pipe(plugins.sass({
-        errLogToConsole: true
-    }))
+    .pipe(plugins.sass({errLogToConsole: true}))
     .pipe(plugins.autoprefixer("last 1 version", "> 1%", "ie 8", "ie 7"))
     .pipe(plugins.size({showFiles: true}))
     .pipe(plugins.minifyCss())
-    // .pipe(plugins.csslint({
-    //     'compatible-vendor-prefixes': false,
-    //     'box-sizing': false,
-    //     'important': false,
-    //     'known-properties': false
-    // }))
-    // .pipe(plugins.csslint.reporter())
     .pipe(plugins.rename({suffix: '.min'}))
     .pipe(plugins.size({showFiles: true}))
     .pipe(gulp.dest(dirs.prod.styles))
-    .pipe(browserSync.reload({stream:true}));
   });
 
   gulp.task('fonts', function() {
@@ -79,7 +67,7 @@ gulp.task('install', function () {
     return gulp.src(dirs.dev.libs)
     .pipe(plugins.concat('libs.min.js'))
     .pipe(plugins.size({showFiles: true}))
-    .pipe(plugins.uglify({mangle: false}))
+    .pipe(plugins.uglifyjs({mangle: false}))
     .pipe(plugins.size({showFiles: true}))
     .pipe(gulp.dest(dirs.prod.scripts));
   });
@@ -89,7 +77,7 @@ gulp.task('install', function () {
     return gulp.src(dirs.dev.js)
     .pipe(plugins.concat('app.min.js'))
     .pipe(plugins.size({showFiles: true}))
-    .pipe(plugins.uglify({mangle: false}))
+    .pipe(plugins.uglifyjs({mangle: false}))
     .pipe(plugins.size({showFiles: true}))
     .pipe(gulp.dest(dirs.prod.scripts));
   });
@@ -111,7 +99,7 @@ gulp.task('install', function () {
 
     gulp.task('img', function() {
       return gulp.src(dirs.dev.img)
-      .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+      // .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
       .pipe(gulp.dest(dirs.prod.images));
     });
 
@@ -120,18 +108,6 @@ gulp.task('install', function () {
       .pipe(plugins.svgmin())
       .pipe(plugins.replace(/_[0-9]+_/g, '')) // Illustrator SVGs; strip appended strings from id names
       .pipe(gulp.dest(dirs.prod.images));
-    });
-
-// ----------------------------------------------------------------
-// Start server & reload on CSS changes
-
-    gulp.task('browser-sync', function() {
-        browserSync.init(null, { server: { baseDir: "./" } });
-    });
-
-    // Function to call for reloading browsers
-    gulp.task('bs-reload', function () {
-        browserSync.reload();
     });
 
 // ----------------------------------------------------------------
@@ -144,18 +120,15 @@ gulp.task('watch', function() {
   gulp.watch(dirs.dev.data,   ['data']);
   //
   gulp.watch(dirs.dev.sass,   ['css']);
-  gulp.watch(dirs.prod.styles,['bs-reload']);
   //
   gulp.watch(dirs.dev.fonts,  ['fonts']);
   gulp.watch(dirs.dev.img,    ['img']);
   gulp.watch(dirs.dev.svg,    ['svg']);
   //
   gulp.watch(dirs.dev.html,   ['html']);
-  gulp.watch(dirs.prod.views, ['bs-reload']);
 });
 
-gulp.task('start', ['bs-reload']);
 gulp.task('assets', ['img', 'svg', 'fonts']);
 gulp.task('build', ['css', 'js', 'libs', 'html', 'data']);
-gulp.task('init', ['install', 'build', 'assets', 'watch', 'start']);
-gulp.task('default', ['build', 'watch', 'start']);
+gulp.task('init', ['install', 'build', 'assets', 'watch']);
+gulp.task('default', ['build', 'watch']);
